1fd9cc421a32a8ba7b62b2349a3572ad145faab4
soulfme <seven11did@gmail.com>
2018-05-06 05:35:50 +0800

    添加新字段，添加阿里云短信API

--- sumbit.php
+++ sumbit.php
@@ -1,4 +1,43 @@
-﻿<?php
+<?php
+namespace Aliyun\DySDKLite\Sms;
+require_once "SignatureHelper.php";
+use Aliyun\DySDKLite\SignatureHelper;
+function sendSms($toname,$fromname,$phone) {
+    $params = array ();
+    $accessKeyId = "阿里云accessKeyId";
+    $accessKeySecret = "阿里云accessKeySecret";
+    $params["PhoneNumbers"] = "$phone";
+    $params["SignName"] = "阿里云短信签名";
+    $params["TemplateCode"] = "阿里云短信CODE";
+    $params['TemplateParam'] = Array (
+        "toname" => "$toname",
+        "fromname" => "$fromname"
+    );
+    $params['OutId'] = "12345";
+    $params['SmsUpExtendCode'] = "1234567";
+    if(!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+        $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+    }
+    $helper = new SignatureHelper();
+    $content = $helper->request(
+        $accessKeyId,
+        $accessKeySecret,
+        "dysmsapi.aliyuncs.com",
+        array_merge($params, array(
+            "RegionId" => "cn-hangzhou",
+            "Action" => "SendSms",
+            "Version" => "2017-05-25",
+        ))
+    );
+    return $content;
+}
+// ini_set("display_errors", "on");
+// error_reporting(E_ALL);
+// set_time_limit(0);
+// header("Content-Type: text/plain; charset=utf-8");
+// print_r(sendSms());
+?>
+<?php
 	include ('conn.php');
 	ob_start();
 	header("Content-type: text/html; charset=utf-8");
@@ -9,10 +48,20 @@
 	$to = $_POST['towho'];
 	$qq = $_POST['qq'];
 	$content = $_POST['content'];
+	$phone = $_POST['phone'];
 	$ip = $_SERVER["REMOTE_ADDR"];
+	$key = $_POST['key'];
 	date_default_timezone_set('Etc/GMT-8');
 	$lastdate = date("Y-m-d H:i");
-	$sql = "insert into list (fromname,toname,content,lastdate,ip,qq)  values('$name','$to','$content','$lastdate','$ip','$qq')";
+	$smsa = "SELECT * FROM `sms` WHERE `key` = '$key'";
+	$smsb = mysqli_query($conn,$smsa);
+	$smsc = mysqli_fetch_array($smsb);
+	if($smsc['money']>0){
+		sendSms($to,$name,$phone);
+		$ok = "update sms set money = money - 1 ";
+		$result = mysqli_query($conn,$ok);
+	}
+	$sql = "insert into list (fromname,toname,content,lastdate,ip,qq,phone)  values('$name','$to','$content','$lastdate','$ip','$qq','$phone')";
 	$result = mysqli_query($conn,$sql);
 	header('Location:index.php');
 ?>
