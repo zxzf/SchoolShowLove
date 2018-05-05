568856fecc01a06c2469e1111434996c22dcbd1b
soulfme <seven11did@gmail.com>
2018-05-06 05:34:55 +0800

    重构数据库

--- db.sql
+++ db.sql
@@ -1,30 +1,66 @@
-﻿SET FOREIGN_KEY_CHECKS = 0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-DROP TABLE IF EXISTS  `list`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 CREATE TABLE `list` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Id` int(11) NOT NULL,
   `fromname` varchar(255) NOT NULL DEFAULT '',
   `toname` varchar(255) NOT NULL DEFAULT '',
   `content` varchar(255) NOT NULL DEFAULT '',
   `lastdate` varchar(255) NOT NULL DEFAULT '',
   `ip` varchar(255) NOT NULL DEFAULT '',
   `qq` varchar(255) NOT NULL DEFAULT '',
-  `phone` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+  `phone` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-insert into `list`(`Id`,`fromname`,`toname`,`content`,`lastdate`,`ip`,`qq`,`phone`) values
-('1','XX','XXXX','XXXX','2017-10-01 08:00','127.0.0.1','10001','10001');
-DROP TABLE IF EXISTS  `system`;
+INSERT INTO `list` (`Id`, `fromname`, `toname`, `content`, `lastdate`, `ip`, `qq`, `phone`) VALUES
+(1, '关键词', '20', '开心吧', '2018-04-30 16:47', '', '', ''),
+;
+
+CREATE TABLE `sms` (
+  `Id` int(11) NOT NULL,
+  `key` varchar(255) NOT NULL DEFAULT '',
+  `money` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+INSERT INTO `sms` (`Id`, `key`, `money`) VALUES
+(1, 'happytest', 29);
 
 CREATE TABLE `system` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL DEFAULT '',
   `titlesm` varchar(255) NOT NULL DEFAULT '',
-  `school` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `school` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-insert into `system`(`Id`,`title`,`titlesm`,`school`) values
-('1','校园表白墙','喜欢就表白吧！','XX学校');
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `system` (`Id`, `title`, `titlesm`, `school`) VALUES
+(1, '校园表白墙', '喜欢就表白吧！', '第一中学');
+
+ALTER TABLE `list`
+  ADD PRIMARY KEY (`Id`);
+
+ALTER TABLE `sms`
+  ADD PRIMARY KEY (`Id`);
+
+ALTER TABLE `system`
+  ADD PRIMARY KEY (`Id`);
+
+ALTER TABLE `list`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `sms`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `system`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
